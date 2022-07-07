4dcdfdffd47817085e74d01bdc8b145cde98348f
Juponesa <julianaotsuka@gmail.com>
2022-07-07 12:02:25 -0300

    add some more projects

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,15 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var img1 = "images/dice" + randomNumber1 + ".png"
+document.querySelector(".img1").setAttribute("src", img1);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var img2 = "images/dice" + randomNumber2 + ".png"
+document.querySelector(".img2").setAttribute("src", img2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🏁 Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🏁";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
